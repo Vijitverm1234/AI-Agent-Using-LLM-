d3107d4f2238cb17d12aff4de5fb5bf7a52d0541
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-08-22 20:48:51 +0530

    DAY 1

---
+++ ai-agent-assistant/inngest/functions/on-signup.js
@@ -0,0 +1,30 @@
+import { NonRetriableError } from "inngest";
+import User from "../../models/user.js";
+import { sendMail } from "../../utils/mailer";
+import { inngest } from "../client";
+export const onUserSignUp=inngest.createFunction(
+    {id:"on-user-signup",retries:2},
+    {event:"user/signup"},
+    async({event,step})=>{
+        try {
+            const {email}=event.data;
+           const user= await step.run("get-user-email",async()=>{
+               const userobj=await User.findOne({email})
+               if(!userobj){
+                throw new NonRetriableError("User No longer exists in our database");
+               } return userobj;
+
+            })
+           await step.run("send-welcome-email",async()=>{
+                   const subject=`welcome to the app`
+                   const message=`Hi, \n \n
+                   Thank you signing in . we are glad to have ya onboard
+                   `
+                   await sendMail(user.email,subject,message)
+           })
+           return {sucess:true}
+        } catch (error) {
+            console.log("🥹 Error occured")
+        }
+    }
+)
