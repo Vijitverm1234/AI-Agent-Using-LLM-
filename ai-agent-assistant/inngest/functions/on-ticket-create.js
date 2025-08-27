e156617710cf3ee23e0b5247da1d1d0d49abd049
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-08-27 20:26:26 +0530

    Making AI pipeline

---
+++ ai-agent-assistant/inngest/functions/on-ticket-create.js
@@ -0,0 +1,67 @@
+import { NonRetriableError } from "inngest";
+import User from "../../models/user.js";
+import { sendMail } from "../../utils/mailer";
+import { inngest } from "../client";
+import Ticket from "../../models/ticket.js";
+import analyzeTicket from "../../utils/ai.js";
+export const onTicketCreated = inngest.createFunction(
+  { id: "on-ticket-created", retries: 2 },
+  { event: "ticket/created" },
+  async ({ event, step }) => {
+    try {
+      const { ticketId } = event.data;
+      //fetching ticket
+      const ticket = await step.run("fetch-ticket", async () => {
+        const ticketObject = await Ticket.findById(ticketId);
+        if (!ticket) {
+          throw new NonRetriableError("Ticket Not Found");
+        }
+        return ticketObject;
+      });
+      await step.run("update-ticket-status", async () => {
+        await Ticket.findByIdAndUpdate(ticket._id, { status: "TODO" });
+      });
+      const aiResponse = await analyzeTicket(ticket);
+     const relatedSkills= await step.run("ai-processing", async () => {
+        let skills = [];
+        if (aiResponse) {
+          await Ticket.findByIdAndUpdate(ticket._id, {
+            priority: !["low", "medium", "high"].includes(aiResponse.priority)
+              ? "medium"
+              : aiResponse.priority, //if in case Ai has not assigned any of priority then we are going to assign it
+             helpfulNotes:aiResponse.helpfulNotes,
+             status:"IN_PROGRESS",
+             relatedSkills:aiResponse.relatedSkills
+            });
+            skills=aiResponse.relatedSkills
+        }
+        return skills
+      });
+      const moderator=await step.run("assign-moderator",async()=>{
+        let user=await User.findOne({
+            role:"moderator",
+            skills:{
+                $elemMatch:{
+                    $regex:relatedSkills.join("|"),
+                    $options:"i"
+                }
+            }
+        })
+        if(!user){
+            user=await User.findOne({
+                role:"admin"
+            })
+        }
+        await Ticket.findByIdAndUpdate(ticket._id,{
+            assignedTo:user?._id || null
+        })
+        return user;
+      })
+      await step.run("send-email-notification",async()=>{
+        if(moderator){
+            await Ticket
+        }
+      })
+    } catch (error) {}
+  }
+);
